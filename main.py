b0e0facbc068768dc1fcb33468f9fd36adcc3c25
Haitao Pan <manbuzhe2009@qq.com>
2023-11-15 18:03:53 +0800

    renamed: src -> lib

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from uvicorn import run
-from example_pkg.core import app
+# main.py
+from example_pkg import core
 
-# 启动 app
+app = core.create_app()
+
 if __name__ == "__main__":
     run(app, host="0.0.0.0", port=80, reload=True)
